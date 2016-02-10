a344112a07b97148b1d4c5b02a0f40199cf3b337
Maurício Aniche <mauricioaniche@gmail.com>
2016-02-10 16:43:40 +0100

    added one more test to safety

--- src/test/java/org/smellycat/analysis/smells/springmvc/repository/FatRepositoryTest.java
+++ src/test/java/org/smellycat/analysis/smells/springmvc/repository/FatRepositoryTest.java
@@ -36,4 +36,13 @@ public class FatRepositoryTest extends SmellTest {
 		SmellyClass sc = repo.getByClass("mfa.t1.InvoiceRepository2");
 		Assert.assertEquals(4, sc.getAttribute("number-of-entities-as-dependencies"));
 	}
+
+	@Test
+	public void countNumberOfEntitiesInReturnType() throws UnsupportedEncodingException {
+		SmellAnalysis tool = new SmellAnalysis(arch, basePath + "fat-repository/t1", ps, repo);
+		tool.run();
+		
+		SmellyClass sc = repo.getByClass("mfa.t1.InvoiceRepository3");
+		Assert.assertEquals(2, sc.getAttribute("number-of-entities-as-dependencies"));
+	}
 }
---
+++ test-repo/fat-repository/t1/InvoiceRepository3.java
@@ -0,0 +1,13 @@
+package mfa.t1;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InvoiceRepository3 {
+	
+	public List<Invoice> add() {}
+
+
+	public Item remove() {}
+
+}
